2d3b7dbf2b6d67f4826bda6c35f75eed0ee18149
inochigakegohan <kishiura6202@gmail.com>
2024-07-10 09:55:59 +0900

    feat: RestaurantとReviewモデルを追加

--- restaurant/restaurant/cms/models.py
+++ restaurant/restaurant/cms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    """飲食店"""
+    name = models.CharField('店名', max_length=255)
+    genre = models.CharField('ジャンル', max_length=255, blank=True)
+    seat_count = models.IntegerField('席数', blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    """レビュー"""
+    restaurant = models.ForeignKey(Restaurant, verbose_name='店名', related_name='reviews', on_delete=models.CASCADE)
+    comment = models.TextField('コメント', blank=True)
+
+    def __str__(self):
+        return self.comment
+
