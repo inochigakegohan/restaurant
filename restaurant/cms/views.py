02bea7bb9c16e5e4f05f0a409aa5fdbfbac7eaa6
inochigakegohan <kishiura6202@gmail.com>
2024-07-13 08:49:52 +0900

    feat: 飲食店の一覧、編集、削除のビューのひな形を追加

--- restaurant/cms/views.py
+++ restaurant/cms/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def restaurant_list(request):
+    """飲食店の一覧"""
+    return HttpResponse('飲食店の一覧')
+
+
+def restaurant_edit(request, restaurant_id=None):
+    """飲食店の編集"""
+    return HttpResponse('飲食店の編集')
+
+
+def restaurant_del(request, restaurant_id):
+    """飲食店の削除"""
+    return HttpResponse('飲食店の削除')
