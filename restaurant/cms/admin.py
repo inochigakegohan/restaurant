4d2081f8212c73831c36c0e57660e3e146cced6e
inochigakegohan <kishiura6202@gmail.com>
2024-07-12 09:47:33 +0900

    feat: 管理画面にRestaurantとReviewモデルの表示設定を追加

--- restaurant/cms/admin.py
+++ restaurant/cms/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from cms.models import Restaurant, Review
 
-admin.site.register(Restaurant)
-admin.site.register(Review)
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'genre', 'seat_count',)  # 一覧に出したい項目
+    list_display_links = ('id', 'name',)  # 修正リンクでクリックできる項目
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment',)
+    list_display_links = ('id', 'comment',)
+    raw_id_fields = ('restaurant',)   # 外部キーをプルダウンにしない（データ件数が増加時のタイムアウトを予防）
+
+
+admin.site.register(Review, ReviewAdmin)
