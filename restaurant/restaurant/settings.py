0e340bfb49ef0aeab728386bd40a848933bb3cf5
inochigakegohan <kishiura6202@gmail.com>
2024-07-13 08:36:15 +0900

    feat: bootstrap4の導入

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cms.apps.CmsConfig',  # cms アプリケーション
+    'bootstrap4',  # django-bootstrap4
 ]
 
 MIDDLEWARE = [
