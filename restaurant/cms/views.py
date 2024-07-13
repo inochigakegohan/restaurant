c630eae043054d83f636aa72caef735cbc32af80
inochigakegohan <kishiura6202@gmail.com>
2024-07-13 09:46:37 +0900

    feat: restaurantの編集ビューを実装

--- restaurant/cms/views.py
+++ restaurant/cms/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from cms.models import Restaurant
+from cms.forms import RestaurantForm
+
 
 def restaurant_list(request):
     """飲食店の一覧"""
@@ -12,7 +14,22 @@ def restaurant_list(request):
 
 def restaurant_edit(request, restaurant_id=None):
     """飲食店の編集"""
-    return HttpResponse('飲食店の編集')
+    # return HttpResponse('飲食店の編集')
+    if restaurant_id:  # restaurant_id が指定されている (修正時)
+        restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+    else:  # restaurant_id が指定されていない (追加時)
+        restaurant = Restaurant()
+
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST, instance=restaurant)  # POST された request データからフォームを作成
+        if form.is_valid():  # フォームのバリデーション
+            restaurant = form.save(commit=False)
+            restaurant.save()
+            return redirect('cms:restaurant_list')
+    else:  # GET の時
+        form = RestaurantForm(instance=restaurant)  # restaurant インスタンスからフォームを作成
+
+    return render(request, 'cms/restaurant_edit.html', dict(form=form, restaurant_id=restaurant_id))
 
 
 def restaurant_del(request, restaurant_id):
