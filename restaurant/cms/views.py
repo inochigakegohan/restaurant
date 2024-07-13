a25ce114ec4134306e7aedefd37485b8b06920ce
inochigakegohan <kishiura6202@gmail.com>
2024-07-13 09:06:42 +0900

    feat: 映画の一覧ビューを実装

--- restaurant/cms/views.py
+++ restaurant/cms/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from cms.models import Restaurant
 
 def restaurant_list(request):
     """飲食店の一覧"""
-    return HttpResponse('飲食店の一覧')
+    restaurants = Restaurant.objects.all().order_by('id')
+    return render(request,
+                  'cms/restaurant_list.html',  # 使用するテンプレート
+                  {'restaurants': restaurants})  # テンプレートに渡す
 
 
 def restaurant_edit(request, restaurant_id=None):
