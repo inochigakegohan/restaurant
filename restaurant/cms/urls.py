1b6523d366595bf81618423722bd05ea2ba10df5
inochigakegohan <kishiura6202@gmail.com>
2024-07-15 09:47:12 +0900

    style: 整形

--- restaurant/cms/urls.py
+++ restaurant/cms/urls.py
@@ -4,7 +4,7 @@ from cms import views
 app_name = 'cms'
 urlpatterns = [
     # 飲食店
-    path('restaurant/', views.restaurant_list, name='restaurant_list'),   # 一覧
+    path('restaurant/', views.restaurant_list, name='restaurant_list'),  # 一覧
     path('restaurant/add/', views.restaurant_edit, name='restaurant_add'),  # 登録
     path('restaurant/mod/<int:restaurant_id>/', views.restaurant_edit, name='restaurant_mod'),  # 修正
     path('restaurant/del/<int:restaurant_id>/', views.restaurant_del, name='restaurant_del'),   # 削除
